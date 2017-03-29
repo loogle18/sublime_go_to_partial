bcb711d88b4dea821647c73b1854b43473e98928
loogle18 <sme@jetthoughts.com>
2017-03-29 23:10:21 +0300

    Fixed bug with same words in partial name and other strings.
        When line contains same words in partial
        and another string, for example:
        = render 'shared/partial', locals: { data: 'shared' }
        And plugin was called when cursor was
        at last 'shared' string, not at 'shared/partial'
        Then plugin will try to open 'shared/partial',
        but it should not do it. It should open only,
        when cursor was at 'shared/partial'.

--- go_to_partial.py
+++ go_to_partial.py
@@ -9,23 +9,38 @@ class GoToPartialCommand(TextCommand):
         view = self.view
         window = view.window()
         region = view.sel()[0]
-        current_line = view.substr(view.line(region))
-        text_at_cursor = view.substr(view.word(region))
-        target_partial_path = self.partial_name(current_line, text_at_cursor)
+        # Only for cases when we have single project
+        project_folder_path = window.folders()[0]
+        target_partial_path = self.partial_name(region)
 
         if target_partial_path:
-            file = self.partial_path(window.folders()[0], target_partial_path)
+            file = self.partial_path(project_folder_path, target_partial_path)
 
             if file:
                 window.open_file(file)
 
-    def partial_name(self, line, text):
-        if 'render' in line:
-            matched_list = findall(r"['\"](.*?)['\"]", line)
+    def partial_name(self, region):
+        view = self.view
+        line_region = view.line(region)
+        content = view.substr(line_region)
 
-            for partial_name in matched_list:
-                if text in partial_name:
-                    return partial_name
+        if 'render' in content:
+            matched_list = findall(r"['\"](.*?)['\"]", content)
+            line_range = list(range(tuple(line_region)[0],
+                                    tuple(line_region)[1] + 1))
+
+            if matched_list:
+                # Partial name is always the first element with quotes, e.g.:
+                # = render partial: 'partial_name', locals: { data: 'name' }
+                partial = matched_list[0]
+                partial_region = view.find(partial, line_range[0])
+                # Both values of region are identical, so we need only one
+                cursor_at = tuple(region)[0]
+                partial_range = list(range(tuple(partial_region)[0],
+                                           tuple(partial_region)[1] + 1))
+
+                if cursor_at in partial_range:
+                    return partial
 
     def partial_path(self, project_dir, partial_name):
         project_dir += '/app/views'
